6e8c90c7b3891466e09f8f9b281050a713a9b1b6
ianwallace0 <52143079+ianwallace0@users.noreply.github.com>
2021-09-01 02:36:39 -0700

    Update game_122650.c
    Fixed formatting and removed temp variables.

--- conker/src/game/done/game_122650.c
+++ conker/src/game/done/game_122650.c
@@ -12,32 +12,22 @@ void func_150F51A0(s32 arg0, s32 arg1) {
 
 s32 func_150F51BC(void) {
     if (D_80088B40 != 0) {
-        D_80088B40 = 0U;
+        D_80088B40 = 0;
         return 1;
+    } else {
+        return 0;
     }
-    return 0;
 }
 
 void func_150F51E8(s32 arg0) {
-    s32 temp_t6;
-    s32 temp_v0;
-    s32 temp_v1;
-    s32 phi_v0;
-
-    temp_t6 = D_800CC454 & 0x1F;
-    if ((temp_t6 == 3) || (temp_t6 == 4)) {
-        temp_v0 = D_800BE9E4;
-        temp_v1 = D_80088B44;
-        phi_v0 = temp_v0;
-
-        if (temp_v0 < temp_v1){
-			if(D_800C35EA != 1) {
-				D_80088B44 = (temp_v1 - temp_v0);
-        	}
-		} else {
-			D_80088B44 = ((func_150ADA20() & 0x3F) + 0xC0);
-			func_10010F30(15, 18000, ((func_150ADA20() & 0x3F) + 0x20), 0, 0);
-		}
-	}
+    if (((D_800CC454 & 0X1F) == 3) || ((D_800CC454 & 0x1F) == 4)) {
+        if (D_800BE9E4 < D_80088B44){
+            if(D_800C35EA != 1) {
+                D_80088B44 = (D_80088B44 - D_800BE9E4);
+            }
+        } else {
+            D_80088B44 = ((func_150ADA20() & 0x3F) + 0xC0);
+            func_10010F30(15, 18000, ((func_150ADA20() & 0x3F) + 0x20), 0, 0);
+        }
+    }
 }
-
